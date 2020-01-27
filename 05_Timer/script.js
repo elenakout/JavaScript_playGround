8105202fc1fe250bc1d22708edd03666d68f5271
elenakout <elenakout@gmail.com>
2020-01-27 22:16:08 +0200

    05/fix this issue

--- 05_Timer/script.js
+++ 05_Timer/script.js
@@ -7,9 +7,9 @@ class Timer {
     this.startButton.addEventListener("click", this.start);
   }
 
-  start() {
+  start = () => {
     console.log("Timer started");
-  }
+  };
 }
 
 const durationInput = document.querySelector("#duration");

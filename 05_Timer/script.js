b2539b027566fd659abd21a81136d1b21bd0d504
elenakout <elenakout@gmail.com>
2020-01-28 16:10:11 +0200

    05/Starting and Pausing the Timer

--- 05_Timer/script.js
+++ 05_Timer/script.js
@@ -5,10 +5,20 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
   }
 
   start = () => {
-    console.log("Timer started");
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    console.log("tick");
   };
 }
 

4ddaa5a9b34d6214cd7b4727234e11f45c030738
elenakout <elenakout@gmail.com>
2020-02-08 20:01:42 +0200

    [movie-fights] 233 Handling Errored Responses

--- 06_Movie-fight/index.js
+++ 06_Movie-fight/index.js
@@ -1,30 +1,33 @@
 const fetchData = async searchTerm => {
-  const response = await axios.get("http://www.omdbapi.com/", {
+  const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: "d034fc1c",
+      apikey: 'd034fc1c',
       s: searchTerm
     }
   });
 
-  console.log(response.data);
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
-const input = document.querySelector("input");
+const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+      `;
+
+    document.querySelector('#target').appendChild(div);
+  }
 };
 
-const onInput = event => {
-  fetchData(event.target.value);
-};
-
-input.addEventListener("input", debounse(onInput));
+input.addEventListener('input', debounce(onInput));

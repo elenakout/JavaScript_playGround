760bbe12ae82b0817e5e03a97e54b6870de831a5
elenakout <elenakout@gmail.com>
2020-02-08 18:46:05 +0200

    [movie fights] 227 Delaying Search Input

--- 06_Movie-fight/index.js
+++ 06_Movie-fight/index.js
@@ -1,1 +1,24 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "d034fc1c",
+      s: searchTerm
+    }
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+let timeoutId;
+const onInput = event => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+
+input.addEventListener("input", onInput);

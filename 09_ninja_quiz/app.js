29f7a5f6af3b9c084053b33770c0d9ea7b4ad240
elenakout <elenakout@gmail.com>
2020-10-06 20:41:46 +0300

    [ninja-quiz] complete

--- 09_ninja_quiz/app.js
+++ 09_ninja_quiz/app.js
@@ -1,28 +1,32 @@
-const form = document.querySelector('.signup-form');
-const feedback = document.querySelector('.feedback');
-const usernamePattern = /^[a-zA-Z]{6,12}$/;
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
-// validation
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  const username = form.username.value;
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
 
-  if (usernamePattern.test(username)) {
-    feedback.textContent = 'that username is valid!'
-  } else {
-    feedback.textContent = 'username must contain only letters & be between 6 & 12 characters';
-  }
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25;
+    }
+  });
+
+  // show the result
+  scrollTo(0, 0);
+  result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+
 });
-
-// live feedback
-form.username.addEventListener('keyup', e => {
-  // console.log(e.target.value, form.username.value);
-  if (usernamePattern.test(e.target.value)) {
-    //console.log('passed');
-    form.username.setAttribute('class', 'success');
-  } else {
-    //console.log('failed');
-    form.username.setAttribute('class', 'error');
-  }
-})
---
+++ 09_ninja_quiz/notes.md
@@ -0,0 +1,1 @@
+# 8. Project - Interactive Ninja Quiz
